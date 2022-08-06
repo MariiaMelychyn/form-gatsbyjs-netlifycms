3ddef4de15ce2f4999e0dc44f6a6cf5a92430a14
MariiaMelychyn <lv080191mmi75@gmail.com>
2022-08-06 23:04:32 +0300

    Update Form.js

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -29,7 +29,7 @@ const IndexPage = () => {
    
       
          return (
-         <form onSubmit={handleSubmit} name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+         <form onSubmit={handleSubmit} name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
            <input type="hidden" name="form-name" value="contact"/>
            <input 
            id="name" 
